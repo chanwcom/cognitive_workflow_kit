92e17fb381c49058285ad6ae7b9a2298e1e2eaa6
Chanwoo Kim <chanwcom@gmail.com>
2024-06-08 22:23:22 +0900

    udpate

--- tool/wav2vec_huggingface.py
+++ tool/wav2vec_huggingface.py
@@ -5,9 +5,11 @@ import os
 # Third-party imports
 from torch.utils import data
 import tensorflow as tf
+import torch
 
 # Custom imports
 from data.format import speech_data_helper
+from typing import Any, Dict, List, Optional, Union
 
 #db_top_dir="/home/chanwcom/databases/"
 db_top_dir = "/home/chanwcom/speech_database"
@@ -15,18 +17,31 @@ train_top_dir = os.path.join(db_top_dir, "stop/music_train_tfrecord")
 test_top_dir = os.path.join(db_top_dir,
                             "stop/test_0_music_random_300_tfrecord")
 
-train_dataset = tf.data.TFRecordDataset(glob.glob(
-    os.path.join(train_top_dir, "*tfrecord-*")),
-                                        compression_type="GZIP")
-train_dataset = train_dataset.batch(10)
-test_dataset = tf.data.TFRecordDataset(glob.glob(
-    os.path.join(test_top_dir, "*tfrecord-*")),
-                                       compression_type="GZIP")
-test_dataset = test_dataset.batch(10)
+# yapf: disable
+op = speech_data_helper.SpeechDataToWave()
+train_dataset = tf.data.TFRecordDataset(
+    glob.glob(os.path.join(train_top_dir, "*tfrecord-*")),
+              compression_type="GZIP")
+train_dataset = train_dataset.batch(1)
+train_dataset = train_dataset.map(op.process)
+# yapf: enable
+
+# yapf: disable
+test_dataset = tf.data.TFRecordDataset(
+    glob.glob(os.path.join(test_top_dir, "*tfrecord-*")),
+              compression_type="GZIP")
+test_dataset = test_dataset.batch(1)
+test_dataset = test_dataset.map(op.process)
+# yapf: enable
+
+def to_torch(inputs: dict):
+    for key in inputs.keys():
+        inputs[key] = torch.tensor(inputs[key].numpy())
+
+    return inputs
 
 
 class IterDataset(data.IterableDataset):
-
     def __init__(self, tf_dataset):
         self._dataset = tf_dataset
         op = speech_data_helper.SpeechDataToWave()
@@ -34,18 +49,28 @@ class IterDataset(data.IterableDataset):
         # The following line is neede, otherwise ..dataset.map will not work
 
         # Parses the serialized data.
-        self._dataset = self._dataset.map(op.process)
+        #self._dataset = self._dataset.map(op.process)
 
     def __iter__(self):
         for data in self._dataset:
             #torch.as_tensor(val.numpy()).to(device)
-            yield (data)
+            output = {}
+            output["input_values"] = tf.squeeze(data[0]["SEQ_DATA"],
+                                                axis=2).numpy()
+            output["input_length"] = tf.squeeze(data[0]["SEQ_LEN"]).numpy()
+            output["labels"] = data[1]["SEQ_DATA"]
+
+            yield (output)
 
 
 iter_dataset = IterDataset(test_dataset)
 
-for data in test_dataset:
-    print(data)
+import pdb
+
+pdb.set_trace()
+
+for elem in iter_dataset:
+    print(elem)
 
 count = 0
 for elem in iter_dataset:
