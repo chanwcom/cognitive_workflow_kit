a4b70d780f081b21127b51f9cda91d431173e5cb
Chanwoo Kim <chanwcom@gmail.com>
2024-07-08 01:35:54 +0900

    update

--- run/wav2vec_finetuning.py
+++ run/wav2vec_finetuning.py
@@ -192,29 +192,27 @@ class MyTrainer(Trainer):
 
 class MyCtcTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
-
-        blank_augmented_inputs = {}
-        blank_augmented_inputs["SEQ_DATA"] = inputs["labels"]
-        blank_augmented_inputs["SEQ_LEN"] = torch.sum(
-            (inputs["labels"] >= 0).type(torch.int32), axis=1)
-
         with torch.device(inputs["input_values"].device.type):
+            blank_augmented_inputs = {}
+            blank_augmented_inputs["SEQ_DATA"] = inputs["labels"]
+            blank_augmented_inputs["SEQ_LEN"] = torch.sum(
+                (inputs["labels"] >= 0).type(torch.int32), axis=1)
             blank_augmented_inputs = seq_loss_util.to_blank_augmented_labels(
-                blank_augmented_inputs, 0, False)
+                blank_augmented_inputs, 0, False, False)
 
-        target = inputs.pop("labels")
-        outputs = model(**inputs)
+            target = inputs.pop("labels")
+            outputs = model(**inputs)
 
-        logits = outputs["logits"]
-        logits_lengths = torch.full(size=(logits.shape[0],), fill_value=logits.shape[1])
+            logits = outputs["logits"]
+            logits_lengths = torch.full(
+                size=(logits.shape[0],), fill_value=logits.shape[1]).to(logits.device)
 
-        target = blank_augmented_inputs["SEQ_DATA"]
-        target_lengths = blank_augmented_inputs["SEQ_LEN"]
+            target = blank_augmented_inputs["SEQ_DATA"]
+            target_lengths = blank_augmented_inputs["SEQ_LEN"]
 
-        with torch.device(inputs["input_values"].device.type):
             ctc_loss = seq_loss_util.CtcLoss()
             loss = ctc_loss.apply(
-                target, target_lengths, logits.log_softmax(2), logits_lengths)
+                target, target_lengths, logits.log_softmax(2), logits_lengths).mean()
 
         if return_outputs:
             return loss, outputs
