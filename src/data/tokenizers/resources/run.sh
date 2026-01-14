526839624d8dc35369da911c3fa38ea1da657106
Chanwoo Kim <chanwcom@gmail.com>
2026-01-15 00:36:01 +0900

    Update

--- src/data/tokenizers/resources/run.sh
+++ src/data/tokenizers/resources/run.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/bash
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+cd "$REPO_ROOT"
+
 python src/data/tokenizers/train_spm.py \
-    --input libri_raw.txt \
+    --input $REPO_ROOT/src/data/tokenizers/resources/libri_raw.txt \
     --output_dir models/asr \
     --name librispeech_bpe \
     --vocab_size 1024 \
