6909fefa5f46b37f000bbe42dd283f7efffcf4fa
Chanwoo Kim <chanwcom@gmail.com>
2026-02-22 11:58:10 +0900

    update

--- setup.py
+++ setup.py
@@ -1,2 +1,29 @@
-from setuptools import setup, find_packages
-setup(name="my_toolkit", version="0.1", packages=find_packages())
+"""A setup configuration for the cognitive_workflow_kit package.
+
+This script allows the package to be installed in editable mode so that
+project modules are available in the PYTHONPATH automatically.
+"""
+
+__author__ = "Chanwoo Kim(chanwcom@gmail.com)"
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="cwk",
+    version="0.1.0",
+    description="A toolkit for cognitive workflow management",
+    author="chanwcom",
+    # find_packages() locates all directories containing an __init__.py file.
+    packages=find_packages(),
+    # Requirements listed here will be installed by pip when this package
+    # is installed.
+    install_requires=[
+        # "numpy>=1.21.0",
+        # "pandas>=1.3.0",
+    ],
+    # Useful for ensuring the code runs on appropriate Python versions.
+    python_requires=">=3.10",
+    include_package_data=True,
+    zip_safe=False,
+)
